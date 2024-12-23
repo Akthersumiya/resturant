cdd017c1cdfd183fb363c08f799fd1b9e4c8071c
Akthersumiya <mstsumiyaakther2004@gmail.com>
2024-12-23 01:43:35 -0800

    first commit

---
+++ resturant.js
@@ -0,0 +1,7 @@
+let main_button = document.getElementById("main_button");
+let main_menu = document.getElementById("main_menu");
+
+main_button.addEventListener("click",()=>{
+    main_menu.classList.toggle("hidden");
+
+})
