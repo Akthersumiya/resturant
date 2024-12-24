0857098527c561cd6280283ffa92a3c0ab77fa98
Akthersumiya <mstsumiyaakther2004@gmail.com>
2024-12-24 00:25:04 -0800

    added aos

--- resturant.js
+++ resturant.js
@@ -1,5 +1,5 @@
-let main_button = document.getElementById("main_button");
-let main_menu = document.getElementById("main_menu");
+let main_button = document.getElementById("main-button");
+let main_menu = document.getElementById("main-menu");
 
 main_button.addEventListener("click",()=>{
     main_menu.classList.toggle("hidden");
